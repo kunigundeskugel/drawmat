7366864dcbc241c60009ed28fe2bd14a597c3bea
kunigundeskugel <kunigundeskugel@gmail.com>
2025-06-14 20:56:00 +0900

    Add PNG export button to top of UI

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -3,9 +3,14 @@ using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
 using System.Collections.Generic;
-
+using System.Diagnostics;
+using System.IO;
 using DrawMat.ViewModels;
 
 namespace DrawMat.Views;
@@ -47,6 +52,22 @@ public partial class MainWindow : Window
         {
             DrawArea.Children.Add(control);
         }
+    }
+    
+    private void OnSaveImageClick(object? sender, RoutedEventArgs e)
+    {
+        var width = (int)DrawArea.Bounds.Width;
+        var height = (int)DrawArea.Bounds.Height;
+
+        if (width == 0 || height == 0)
+            return;
+
+        var rtb = new RenderTargetBitmap(new PixelSize(width, height));
+        rtb.Render(DrawArea);
+
+        var fileName = "canvas_output.png";
+        using var stream = File.Create(fileName);
+        rtb.Save(stream);
     }
 }
 
