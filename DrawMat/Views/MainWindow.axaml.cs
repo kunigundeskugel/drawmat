837ba9a26f1bfe42269a2aec383153ea548cc7c0
kunigundeskugel <kunigundeskugel@gmail.com>
2025-06-23 18:07:52 +0900

    Implement mode-switching via polyline and selection buttons

--- DrawMat/Views/MainWindow.axaml.cs
+++ DrawMat/Views/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Collections;
 using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Media;
 using DrawMat.ViewModels;
 using System.Collections.Generic;
@@ -11,29 +12,64 @@ namespace DrawMat.Views;
 
 public partial class MainWindow : Window
 {
+    private enum Mode { None, DrawingPolyline, Selection }
+    private Mode currentMode = Mode.None;
     public MainViewModel ViewModel => (MainViewModel)DataContext!;
 
     public MainWindow()
     {
         InitializeComponent();
         DataContext = new MainViewModel();
+        DrawPolylineButton.Click += DrawPolylineButton_Click;
+        SelectButton.Click += SelectButton_Click;
+    }
+
+    private void DrawPolylineButton_Click(object? sender, RoutedEventArgs e)
+    {
+        currentMode = Mode.DrawingPolyline;
+    }
+
+    private void SelectButton_Click(object? sender, RoutedEventArgs e)
+    {
+        currentMode = Mode.Selection;
     }
 
     private void Canvas_PointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        ViewModel.StartPolyline(e.GetPosition(DrawArea));
+        if (currentMode == Mode.DrawingPolyline)
+        {
+            ViewModel.StartPolyline(e.GetPosition(DrawArea));
+        }
+        else if (currentMode == Mode.Selection)
+        {
+            //TODO: implement selection
+        }
         Redraw();
     }
 
     private void Canvas_PointerMoved(object? sender, PointerEventArgs e)
     {
-        ViewModel.ExtendPolyline(e.GetPosition(DrawArea));
+        if (currentMode == Mode.DrawingPolyline)
+        {
+            ViewModel.ExtendPolyline(e.GetPosition(DrawArea));
+        }
+        else if (currentMode == Mode.Selection)
+        {
+            //TODO: implement selection
+        }
         Redraw();
     }
 
     private void Canvas_PointerReleased(object? sender, PointerReleasedEventArgs e)
     {
-        ViewModel.FinishPolyline();
+        if (currentMode == Mode.DrawingPolyline)
+        {
+            ViewModel.FinishPolyline();
+        }
+        else if (currentMode == Mode.Selection)
+        {
+            //TODO: implement selection
+        }
         Redraw();
     }
 
